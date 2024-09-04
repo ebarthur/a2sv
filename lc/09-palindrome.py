252762a36a30cbb42a4b35356ce881e57dbde05e
lasagna <arthurebenezer@aol.com>
2024-09-04 12:51:39 +0000

    progress questions

---
+++ lc/09-palindrome.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/palindrome-number/submissions/1378758844
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        str_num = str(x)
+
+        return str_num == str_num[::-1]
